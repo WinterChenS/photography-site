9a8380ee9c7fbe140173b61c5cbe1d495e54644e
winterchen <1085143002@qq.com>
2021-01-21 18:45:13 +0800

    修复权限问题

--- service/src/main/java/com/winterchen/service/ServiceApplication.java
+++ service/src/main/java/com/winterchen/service/ServiceApplication.java
@@ -1,8 +1,10 @@
 package com.winterchen.service;
 
+import com.winterchen.common.utils.SpringContextHolder;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"com.winterchen"})
 @MapperScan({"com.winterchen.service.modules.admin.mapper",
@@ -15,4 +17,8 @@ public class ServiceApplication {
         SpringApplication.run(ServiceApplication.class, args);
     }
 
+    @Bean
+    public SpringContextHolder springContextHolder() {
+        return new SpringContextHolder();
+    }
 }
